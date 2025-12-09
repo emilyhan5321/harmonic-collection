6b21d6ac90fe39c61b1befa64403dc5972c54f9b
emilyhan5321 <hane316@newschool.edu>
2025-12-09 13:58:05 +0000

    final

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ document.querySelector(".main-content");
 let changeColor = () => {
 
 }
+
+
+document.addEventListener("click", function(e) {
+  if (e.target.id === "nav") return;
+  window.location.href = "/1home/index.html"; 
+});
