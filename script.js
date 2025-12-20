9c83b7e390c0aa20a6dce6dc9dc2cf65c971a7d5
emilyhan5321 <hane316@newschool.edu>
2025-12-20 05:40:01 +0000

    final

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ document.addEventListener("click", function(e) {
   if (e.target.id === "nav") return;
   window.location.href = "../1home/index.html"; 
 });
+
+
+document.addEventListener("click", function (e) {
+  // If the click happened inside the nav, do nothing
+  if (e.target.closest("#nav")) return;
+
+  // Otherwise go to next page
+  window.location.href = "../1home/index.html";
+});
